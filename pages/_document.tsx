c2595455be6378bfb118075836c4fcc85e683ad3
jahanzaibsheikh830 <jahanzaibsheikh830@gmail.com>
2021-09-22 18:52:05 +0500

    modified types of variables and props

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,38 @@
+// import Document, { Html, Head, Main, NextScript } from "next/document";
+// import flush from "styled-jsx/server";
+
+// export default class CustomDocument extends Document {
+//   static getInitialProps({ renderPage }) {
+//     const { html, head, errorHtml, chunks } = renderPage();
+//     const styles = flush();
+
+//     return { html, head, errorHtml, chunks, styles };
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <script
+//             src='https://code.jquery.com/jquery-3.6.0.slim.js'
+//             integrity='sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY='
+//             crossOrigin='anonymous'
+//           ></script>
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import flush from "styled-jsx/server";
 
-export default class CustomDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage();
-    const styles = flush();
-
-    return { html, head, errorHtml, chunks, styles };
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
@@ -27,3 +53,5 @@ export default class CustomDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
