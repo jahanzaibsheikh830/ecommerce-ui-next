16ed6848bae42f2cfa62089993d20c1e20fd68dc
jahanzaibsheikh830 <jahanzaibsheikh830@gmail.com>
2021-10-14 04:58:41 +0500

    changes

--- Component/CardItems/FeaturedAndRatingMain.tsx
+++ Component/CardItems/FeaturedAndRatingMain.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "../Shared/Container";
 import RatingCardItem from "./RatingCardItem";
 import FeaturedCardItem from "./FeaturedCardItem";
-import styles from "../../styles/Components/CardItems/FeaturedAndRatingMain.module.scss";
+import styles from "../../Styles/Components/CardItems/FeaturedAndRatingMain.module.scss";
 export default function FeaturedAndRatingMain() {
   return (
     <div style={{ backgroundColor: "#f6f9fc" }}>
